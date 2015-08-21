a0ed0e00c79bba249532f795b2aafc2a8dbc9383
Scot Hall <xnendron@yahoo.com>
2015-08-20 20:52:56 -0700

    Added timestamp

--- EgitTest/src/halls/scot/egittest/Main.java
+++ EgitTest/src/halls/scot/egittest/Main.java
@@ -3,7 +3,7 @@ package halls.scot.egittest;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Egit Test");
+		System.out.println("Egit Test " + System.currentTimeMillis());
 
 	}
 
